c4025ad8110fcb31daea7806aebc6d018e588556
Gregory Mullen <_@gr.ht>
2023-03-06 15:16:37 -0800

    update screenshot.sh

--- .bin/screenshot.sh
+++ .bin/screenshot.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env sh
+SSHMOUNT="/home/grayhatter/i"
+SITE="https://gr.ht/i"
 
 
 grim -g "`slurp`" /tmp/screenshot.png
 
-
 if [ $# -gt "0" ]; then
-	PREV=`wl-paste`
-	wl-copy -of "/tmp/screenshot.png"
-	wl-copy "${PREV}"
+	if mount | grep "$SSHMOUNT"; then
+		echo "true"
+		cp /tmp/screenshot.png "${SSHMOUNT}/${1}.png"
+		wl-copy -of "${SITE}/${1}.png"
+	else 
+		PREV=`wl-paste`
+		wl-copy -of "/tmp/screenshot.png"
+		wl-copy "${PREV}"
+	fi
 fi
